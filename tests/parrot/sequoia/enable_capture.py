9b5b7e27c5a38262719ba53539c6f2524c433766
M. Domenzain <ld@airinov.fr>
2016-05-03 16:42:03 +0200

    Refactor and check for CaptureCompleted in test.

--- tests/parrot/sequoia/enable_capture.py
+++ tests/parrot/sequoia/enable_capture.py
@@ -1,5 +1,6 @@
 #! /usr/bin/env python
 from ptpy.usb_transport import USBTransport, find_usb_cameras
+# TODO: Fix import once ptpy module is better structured.
 from time import sleep, time
 from tqdm import tqdm
 
@@ -18,9 +19,24 @@ else:
 # cameras activated. Do this for all combinations of activated sensors.
 number_of_cameras = 5
 
-for mask in tqdm(range(2**number_of_cameras), unit='test'):
-    tqdm.write('-------------------------------------------------')
-    tqdm.write('Sending PhotoSensorEnableMask {}'.format(bin(mask)))
+
+def initiate_capture():
+    '''Initiate capture.'''
+    capture_response = sequoia.initiate_capture()
+    # If the device is donig something else, try again ten times waiting a
+    # second.
+    tries = 0
+    while capture_response.ResponseCode != 'OK' and tries < 10:
+        tries += 1
+        sleep(1)
+        capture_response = sequoia.initiate_capture()
+    if capture_response.ResponseCode != 'OK':
+        tqdm.write(capture_response)
+        raise Exception('Could not InitiateCapture')
+
+
+def set_valid_mask(mask):
+    '''Set PhotoSensorEnableMask. Return false when invalid.'''
     enable_response = sequoia.set_device_prop_value(
         'PhotoSensorEnableMask',
         sequoia._UInt32('Mask').build(mask)
@@ -28,13 +44,12 @@ for mask in tqdm(range(2**number_of_cameras), unit='test'):
     # If the combination of enabled cameras is invalid, skip it.
     if enable_response.ResponseCode == 'InvalidDevicePropValue':
         tqdm.write('{} is an invalid mask. Skipping it.'.format(bin(mask)))
-        continue
+        return False
     # If the device is busy, try again ten times waiting a second.
     tries = 0
     while enable_response.ResponseCode != 'OK' and tries < 10:
         tries += 1
         sleep(1)
-        tqdm.write('Sending PhotoSensorEnableMask {}'.format(bin(mask)))
         enable_response = sequoia.set_device_prop_value(
             'PhotoSensorEnableMask',
             sequoia._UInt32('Mask').build(mask)
@@ -45,37 +60,52 @@ for mask in tqdm(range(2**number_of_cameras), unit='test'):
             'Could not set PhotoSensorEnableMask {}'
             .format(bin(mask))
         )
+    return True
 
-    # Capture image and count the ObjectAdded events.
-    tqdm.write('Sending InitiateCapture')
-    sequoia.initiate_capture()
-    acquired = 0
-    n_added = 0
-    expected = bin(mask).count('1')
-    tic = time()
-    failed = False
-    while acquired < expected:
-        evt = sequoia.event()
-        if evt and evt.EventCode == 'ObjectAdded':
-            n_added += 1
-            tqdm.write('Sending GetObjectInfo')
-            info = sequoia.get_object_info(evt.Parameter[0])
-            # tqdm.write(str(info))
-            if info and 'TIFF' in info.ObjectFormat or 'EXIF_JPEG' in info.ObjectFormat:
-                acquired += 1
-        elif evt:
-            tqdm.write('Received event {}'.format(evt.EventCode))
-        # Allow for one minute delays in events...
-        if time() - tic > 60:
+with sequoia.session():
+    for mask in tqdm(range(2**number_of_cameras), unit='test'):
+        tqdm.write('-------------------------------------------------')
+        tqdm.write('Testing capture for mask {}'.format(bin(mask)))
+
+        # If mask is invalid, skip.
+        if not set_valid_mask(mask):
+            continue
+        # Capture image and count the ObjectAdded events.
+        initiate_capture()
+        acquired = 0
+        n_added = 0
+        expected = bin(mask).count('1')
+        tic = time()
+        failed = False
+        while acquired < expected:
+            # Check events
+            evt = sequoia.event()
+            # If object added verify is it is an image
+            if evt and evt.EventCode == 'ObjectAdded':
+                n_added += 1
+                info = sequoia.get_object_info(evt.Parameter[0])
+                if (
+                        info and
+                        ('TIFF' in info.ObjectFormat or
+                         'EXIF_JPEG' in info.ObjectFormat)
+                ):
+                    acquired += 1
+            # Otherwise if the capture is complete, tally up.
+            elif evt and evt.EventCode == 'CaptureCompleted':
+                failed = acquired < expected
+                break
+            # Allow for one-minute delays in events...
+            if time() - tic > 60:
+                failed = True
+                break
+        if failed:
             tqdm.write(
                 'Waited for 1 minute before giving up. '
                 'Failed with {} ({} ObjectAdded) images for mask {}'
                 .format(acquired, n_added, bin(mask))
             )
-            failed = True
-            break
-    if not failed:
-        tqdm.write(
-            'Success: {} images for mask {}'
-            .format(acquired, bin(mask))
-        )
+        else:
+            tqdm.write(
+                'Success: {} images for mask {}'
+                .format(acquired, bin(mask))
+            )

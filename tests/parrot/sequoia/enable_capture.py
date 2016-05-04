ee3abb7cf06a5c5633fe38c4abed9c6f8429f1d8
M. Domenzain <ld@airinov.fr>
2016-05-04 12:24:44 +0200

    Modify test for pytest.

--- tests/parrot/sequoia/enable_capture.py
+++ tests/parrot/sequoia/enable_capture.py
@@ -2,17 +2,17 @@
 from ptpy.usb_transport import USBTransport, find_usb_cameras
 # TODO: Fix import once ptpy module is better structured.
 from time import sleep, time
-from tqdm import tqdm
+import pytest
 
 devs = find_usb_cameras()
 
+sequoia = None
 for dev in devs:
-    sequoia = USBTransport(dev)
-    device_info = sequoia.get_device_info()
+    camera = USBTransport(dev)
+    device_info = camera.get_device_info()
     if 'Sequoia' in device_info.Model:
+        sequoia = camera
         break
-else:
-    raise Exception('No available Sequoia found through PTP.')
 
 
 # Verify that there are at least N images added after an InitiateCapture with N
@@ -31,8 +31,9 @@ def initiate_capture():
         sleep(1)
         capture_response = sequoia.initiate_capture()
     if capture_response.ResponseCode != 'OK':
-        tqdm.write(str(capture_response))
-        raise Exception('Could not InitiateCapture')
+        print('Could not initiate capture:')
+        print(capture_response)
+        assert(capture_response.ResponseCode == 'OK')
 
 
 def set_valid_mask(mask):
@@ -43,7 +44,7 @@ def set_valid_mask(mask):
     )
     # If the combination of enabled cameras is invalid, skip it.
     if enable_response.ResponseCode == 'InvalidDevicePropValue':
-        tqdm.write('{} is an invalid mask. Skipping it.'.format(bin(mask)))
+        print('{} is an invalid mask. Skipping it.'.format(bin(mask)))
         return False
     # If the device is busy, try again ten times waiting a second.
     tries = 0
@@ -55,21 +56,30 @@ def set_valid_mask(mask):
             sequoia._UInt32('Mask').build(mask)
         )
     if enable_response.ResponseCode != 'OK':
-        tqdm.write(str(enable_response))
-        raise Exception(
+        print(str(enable_response))
+        print(
             'Could not set PhotoSensorEnableMask {}'
             .format(bin(mask))
         )
+        assert(enable_response.ResponseCode == 'OK')
     return True
 
-with sequoia.session():
-    for mask in tqdm(range(2**number_of_cameras), unit='test'):
-        tqdm.write('-------------------------------------------------')
-        tqdm.write('Testing capture for mask {}'.format(bin(mask)))
+
+@pytest.mark.parameterize(
+    ('mask'),
+    range(2**number_of_cameras),
+)
+def test_enable_capture(mask):
+    '''Verify that a capture with N enabled sensors poduces N images.'''
+    if sequoia is None:
+        return
+
+    with sequoia.session():
+        print('Testing capture for mask {}'.format(bin(mask)))
 
         # If mask is invalid, skip.
         if not set_valid_mask(mask):
-            continue
+            return
         # Capture image and count the ObjectAdded events.
         initiate_capture()
         acquired = 0
@@ -87,27 +97,29 @@ with sequoia.session():
                 if (
                         info and
                         ('TIFF' in info.ObjectFormat or
-                         'EXIF_JPEG' in info.ObjectFormat)
+                            'EXIF_JPEG' in info.ObjectFormat)
                 ):
                     acquired += 1
             # Otherwise if the capture is complete, tally up.
             elif evt and evt.EventCode == 'CaptureCompleted':
                 failed = acquired < expected
                 break
-            # Allow for one-minute delays in events... Though the asynchronous
-            # event may take an indefinite amount of time, anything longer than
-            # about ten seconds indicates there's something wrong.
+            # Allow for one-minute delays in events... Though the
+            # asynchronous event may take an indefinite amount of time,
+            # anything longer than about ten seconds indicates there's
+            # something wrong.
             if time() - tic > 60:
                 failed = True
                 break
+        assert(not failed)
         if failed:
-            tqdm.write(
+            print(
                 'Waited for 1 minute before giving up. '
                 'Failed with {} ({} ObjectAdded) images for mask {}'
                 .format(acquired, n_added, bin(mask))
             )
         else:
-            tqdm.write(
+            print(
                 'Success: {} images for mask {}'
                 .format(acquired, bin(mask))
             )

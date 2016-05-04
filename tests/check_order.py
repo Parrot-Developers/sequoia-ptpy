95630419f1cf4a03384d737afe4ec22eceb14ae0
M. Domenzain <ld@airinov.fr>
2016-05-04 10:33:05 +0200

    Change test for pytest framework.

--- tests/check_order.py
+++ tests/check_order.py
@@ -7,33 +7,21 @@ camera = USBTransport()
 with camera.session():
     tic = time()
     # Clear all events for ten seconds.
+    print('Clearing all events')
     while time() - tic < 10:
         evt = camera.event()
     print('Initiating capture')
     capture = camera.initiate_capture()
-    evts = []
+    codes = []
     tic = time()
-    print('Waiting for events 10s.')
+    print('Waiting for capture events 10s')
     while time() - tic < 10:
         evt = camera.event()
         if evt:
             print evt
             if evt.TransactionID == capture.TransactionID:
-                evts.append(evt)
+                codes.append(evt.EventCode)
 
-    codes = [x.EventCode for x in evts]
-    print codes
-
-    if 'CaptureComplete' not in codes:
-        raise Exception('No CaptureComplete received.')
-
-    if 'ObjectAdded' not in codes:
-        raise Exception('No ObjectAdded received.')
-
-    if codes.index('ObjectAdded') > codes.index('CaptureComplete'):
-        raise Exception(
-            'Incorrect order: '
-            'ObjectAdded after CaptureCompleted for same transaction.'
-        )
-
-    print('Success!')
+    assert('CaptureComplete' in codes)
+    assert('ObjectAdded' in codes)
+    assert(codes.index('ObjectAdded') < codes.index('CaptureComplete'))

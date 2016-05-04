2eafc768cf7137d78d4bbf57a2d39ca470c8ae69
M. Domenzain <ld@airinov.fr>
2016-05-04 10:41:00 +0200

    Refactor test into function for auto discovery.

--- tests/check_order_test.py
+++ tests/check_order_test.py
@@ -3,25 +3,27 @@ from ptpy.usb_transport import USBTransport
 # TODO Fix import once ptpy module is better structured.
 from time import time
 
-camera = USBTransport()
-with camera.session():
-    tic = time()
-    # Clear all events for ten seconds.
-    print('Clearing all events')
-    while time() - tic < 10:
-        evt = camera.event()
-    print('Initiating capture')
-    capture = camera.initiate_capture()
-    codes = []
-    tic = time()
-    print('Waiting for capture events 10s')
-    while time() - tic < 10:
-        evt = camera.event()
-        if evt:
-            print evt
-            if evt.TransactionID == capture.TransactionID:
-                codes.append(evt.EventCode)
 
-    assert('CaptureComplete' in codes)
-    assert('ObjectAdded' in codes)
-    assert(codes.index('ObjectAdded') < codes.index('CaptureComplete'))
+def test_order():
+    camera = USBTransport()
+    with camera.session():
+        tic = time()
+        # Clear all events for ten seconds.
+        print('Clearing all events')
+        while time() - tic < 10:
+            evt = camera.event()
+        print('Initiating capture')
+        capture = camera.initiate_capture()
+        codes = []
+        tic = time()
+        print('Waiting for capture events 10s')
+        while time() - tic < 10:
+            evt = camera.event()
+            if evt:
+                print evt
+                if evt.TransactionID == capture.TransactionID:
+                    codes.append(evt.EventCode)
+
+        assert('CaptureComplete' in codes)
+        assert('ObjectAdded' in codes)
+        assert(codes.index('ObjectAdded') < codes.index('CaptureComplete'))

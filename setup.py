79f332de549c166ae84406552df12f95457e731f
M. Domenzain <ld@airinov.fr>
2016-05-04 11:02:49 +0200

    Add separate requirements for tests and install

--- setup.py
+++ setup.py
@@ -1,9 +1,18 @@
 import os
 from setuptools import setup, find_packages
+from pip.req import parse_requirements
 
 
 def read(filename):
     return open(os.path.join(os.path.dirname(__file__), filename)).read()
+
+install_requirements = [
+    str(r.req) for r in parse_requirements('requirements.txt', session=False)
+]
+
+test_requirements = [
+    str(r.req) for r in parse_requirements('tests/requirements.txt', session=False)
+]
 
 setup(
     name='ptpy',
@@ -14,4 +23,6 @@ setup(
     keywords='ptp mtp usb ip ptpip ptpusb parrot canon nikon microsoft',
     author='Luis Mario Domenzain',
     author_email='ld@airinov.fr',
+    install_requires=install_requirements,
+    tests_require=test_requirements,
 )

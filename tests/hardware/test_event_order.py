2bb7e9967376a83c5c8f7aededad07daf1890f47
M. Domenzain <ld@airinov.fr>
2016-05-06 12:22:44 +0200

    Refactor event order test to use common fixture.

--- tests/hardware/test_event_order.py
+++ tests/hardware/test_event_order.py
@@ -1,40 +1,39 @@
-from ptpy.usb_transport import USBTransport
 # TODO Fix import once ptpy module is better structured.
 from time import time
+from test_camera import TestCamera
 import pytest
 
-camera = None
-try:
-    camera = USBTransport()
-except Exception:
-    pass
 
+class TestCapture(TestCamera):
+    def test_order(self, cam):
+        device_info = cam.get_device_info()
+        print device_info
+        if 'InitiateCapture' not in device_info.OperationsSupported:
+            pytest.skip('Capture is not supported by camera.')
 
-@pytest.mark.skipif(camera is None, reason='No camera available to test')
-def test_order():
-    with camera.session():
-        tic = time()
-        print('Clearing all events (10s)')
-        while time() - tic < 10:
-            evt = camera.event()
-        print('Initiating capture')
-        capture = camera.initiate_capture()
-        codes = []
-        tic = time()
-        print('Waiting for capture events (10s)')
-        while time() - tic < 10:
-            evt = camera.event()
-            if evt:
-                print evt
-                if evt.TransactionID == capture.TransactionID:
-                    codes.append(evt.EventCode)
+        with cam.session():
+            tic = time()
+            print('Clearing all events (10s)')
+            while time() - tic < 10:
+                evt = cam.event()
+            print('Initiating capture')
+            capture = cam.initiate_capture()
+            codes = []
+            tic = time()
+            print('Waiting for capture events (10s)')
+            while time() - tic < 10:
+                evt = cam.event()
+                if evt:
+                    print evt
+                    if evt.TransactionID == capture.TransactionID:
+                        codes.append(evt.EventCode)
 
-        assert 'CaptureComplete' in codes, 'No CaptureComplete received.'
-        assert 'ObjectAdded' in codes,\
-            'No ObjectAdded received for capture transaction.'
-        capture_complete_index = codes.index('CaptureComplete')
-        last_object_added_index = (
-            (len(codes) - 1) - codes[::-1].index('ObjectAdded')
-        )
-        assert last_object_added_index < capture_complete_index,\
-            'ObjectAdded happened after CaptureComplete.'
+            assert 'CaptureComplete' in codes, 'No CaptureComplete received.'
+            assert 'ObjectAdded' in codes,\
+                'No ObjectAdded received for capture transaction.'
+            capture_complete_index = codes.index('CaptureComplete')
+            last_object_added_index = (
+                (len(codes) - 1) - codes[::-1].index('ObjectAdded')
+            )
+            assert last_object_added_index < capture_complete_index,\
+                'ObjectAdded happened after CaptureComplete.'

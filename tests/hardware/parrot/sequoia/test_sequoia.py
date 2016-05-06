c8f367cf40f6578d5601b6231f7832c0879bd386
Luis Mario Domenzain <domenzain@tl081.fr>
2016-05-06 22:17:39 +0200

    Skip Sequoia tests by fixture.

--- tests/hardware/parrot/sequoia/test_sequoia.py
+++ tests/hardware/parrot/sequoia/test_sequoia.py
@@ -2,12 +2,11 @@ from ...test_camera import TestCamera
 import pytest
 
 
+@pytest.mark.incremental
 class TestSequoia(TestCamera):
-    def test_model(self, camera):
+    @pytest.fixture(autouse=True)
+    def sequoia(self, camera):
         device_info = camera.get_device_info()
         if 'Sequoia' not in device_info.Model:
             pytest.skip('The camera is not a Sequoia')
-
-    @pytest.fixture()
-    def sequoia(self, camera):
         return camera

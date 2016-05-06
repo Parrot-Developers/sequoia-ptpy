00c88eb3159edf8e5da5be677684985eabe31639
M. Domenzain <ld@airinov.fr>
2016-05-06 17:59:49 +0200

    Rename fixture cam->camera.
    There is no problem with the readability or verbosity of the more
    explicit name, so keep it.

--- tests/hardware/test_camera.py
+++ tests/hardware/test_camera.py
@@ -2,15 +2,15 @@ import pytest
 
 
 @pytest.mark.incremental
-@pytest.mark.usefixtures('cam')
+@pytest.mark.usefixtures('camera')
 class TestCamera:
     '''Perform tests on a physical camera.'''
-    def test_device_info(self, cam):
+    def test_device_info(self, camera):
         '''Camera responds to PTP.'''
-        if cam is None:
+        if camera is None:
             pytest.skip('No camera available to test')
 
-        device_info = cam.get_device_info()
+        device_info = camera.get_device_info()
         assert device_info, 'There is no response for DeviceInfo.\n'\
             'The camera may not support PTP.'
         # TODO: Check for mandatory  operations and properties according to PTP

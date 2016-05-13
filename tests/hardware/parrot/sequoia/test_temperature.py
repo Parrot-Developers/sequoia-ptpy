78c0a0ad1d462289d65087bd2e732ba383cf3056
M. Domenzain <ld@airinov.fr>
2016-05-13 18:09:38 +0200

    Be more informative about temperature sensor.

--- tests/hardware/parrot/sequoia/test_temperature.py
+++ tests/hardware/parrot/sequoia/test_temperature.py
@@ -1,11 +1,13 @@
 from test_sequoia import TestSequoia
 
 
-class TestOperations(TestSequoia):
+class TestOperationValues(TestSequoia):
+    '''Check values for different operations.'''
     def test_temperature(self, sequoia):
         '''Verify temperature values are reasonable.'''
         with sequoia.session():
             temperatures = sequoia.get_temperature_values()
             for t in temperatures:
+                print('Checking {} temperature.'.format(t))
                 assert temperatures[t] <= 200000,\
                     'A temperature over 200C means something\'s gone wrong.'

5a6a99f19227d3a4aa6747d098165322a5adbd36
M. Domenzain <ld@airinov.fr>
2016-05-19 11:50:24 +0200

    Add test for low temperatures too.
    The only temperature value allowed is -999C, and it represents a sensor
    error.

--- tests/hardware/parrot/sequoia/test_operation_values.py
+++ tests/hardware/parrot/sequoia/test_operation_values.py
@@ -3,7 +3,7 @@ from test_sequoia import TestSequoia
 
 class TestOperationValues(TestSequoia):
     '''Check values for different operations.'''
-    def test_temperature(self, sequoia):
+    def test_hotness(self, sequoia):
         '''Verify temperature values are reasonable.'''
         with sequoia.session():
             temperatures = sequoia.get_temperature_values()
@@ -11,3 +11,14 @@ class TestOperationValues(TestSequoia):
                 print('Checking {} temperature.'.format(t))
                 assert temperatures[t] <= 200000,\
                     'A temperature over 200C means something\'s gone wrong.'
+
+    def test_absolute_zero(self, sequoia):
+        '''Verify temperature values are reasonable.'''
+        with sequoia.session():
+            temperatures = sequoia.get_temperature_values()
+            for t in temperatures:
+                print('Checking {} temperature.'.format(t))
+                # -999 degrees C means invalid value.
+                if temperatures[t] != -999000:
+                    assert temperatures[t] > -273150,\
+                        'Reported temperature is under absolute zero...'

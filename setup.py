91e9a19dfb346af789f3c19807024303d620a674
M. Domenzain <luis.domenzain@parrot.com>
2018-06-05 13:56:29 +0200

    Avoid importing pip internals

--- setup.py
+++ setup.py
@@ -1,22 +1,11 @@
 import os
 from setuptools import setup, find_packages
-from pip.req import parse_requirements
 
 
 def read(filename):
     with open(os.path.join(os.path.dirname(__file__), filename)) as f:
         return f.read()
 
-
-install_requirements = [
-    str(r.req)
-    for r in parse_requirements('requirements.txt', session=False)
-]
-
-test_requirements = [
-    str(r.req)
-    for r in parse_requirements('tests/requirements.txt', session=False)
-]
 
 setup(
     author_email='ld@airinov.fr',
@@ -43,14 +32,14 @@ setup(
         'Topic :: System :: Hardware :: Hardware Drivers',
     ],
     description='A pure Python implementation of Picture Transfer Protocol.',
-    install_requires=install_requirements,
+    install_requires=read('requirements.txt'),
     keywords='ptp mtp usb ip ptpip ptpusb parrot canon nikon microsoft',
     license='BSD-3-Clause',
     long_description=read('README.md'),
     name='ptpy',
     packages=find_packages(exclude=['tests', 'examples']),
     setup_requires=['pytest-runner'],
-    tests_require=test_requirements,
+    tests_require=read('tests/requirements.txt'),
     url='https://github.com/Parrot-Developers/sequoia-ptpy',
     version='0.3.3',
 )
